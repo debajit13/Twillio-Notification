36e125e4a3827e12b0911751652b67198bcc4e3d
debajit13 <mallickdebajit3@gmail.com>
2020-07-19 19:12:04 +0530

    UPDATED: default messages added to all the variable datas of phone_calls.py and twillio_sms.py

--- phone_calls.py
+++ phone_calls.py
@@ -3,19 +3,19 @@ from twilio.rest import Client
 
 # Twilio phone number goes here. Grab one at https://twilio.com/try-twilio
 # and use the E.164 format, for example: "+12025551234"
-TWILIO_PHONE_NUMBER = "+1 323 870 1850"
+TWILIO_PHONE_NUMBER = "Your Twillio Phone Number"
 
 # list of one or more phone numbers to dial, in "+19732644210" format
-DIAL_NUMBERS = ["+918768881443"]
+DIAL_NUMBERS = ["Numbers Your want to call"]
 
 # URL location of TwiML instructions for how to handle the phone call
 TWIML_INSTRUCTIONS_URL = \
-    "https://a.uguu.se/tYkuK3scn2XF_flood.xml"
+    "Message you want to hear, put it in a xml file and paste the file link here"
 
 # replace the placeholder values with your Account SID and Auth Token
 # found on the Twilio Console: https://www.twilio.com/console
 client = Client(
-    "AC6c8a8f25987021640c13e9548d3a6c10", "2dd710ce2d9ca1930d42ede8fe561975")
+    "Enter Account SID here", "Ener Auth Token here")
 
 
 def dial_numbers(numbers_list):
--- twillio_sms.py
+++ twillio_sms.py
@@ -2,15 +2,15 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'AC6c8a8f25987021640c13e9548d3a6c10'
-auth_token = '2dd710ce2d9ca1930d42ede8fe561975'
+account_sid = 'Enter Your SID here'
+auth_token = 'Enter Your Auth Token here'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
     .create(
-        from_='+13238701850',
-        to='+918768881443',
-        body="Flood alert"
+        from_='Your Twillio Number',
+        to='Number where you want to send SMS',
+        body="Your Message"
     )
 
 print(message.sid)
